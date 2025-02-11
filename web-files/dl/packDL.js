4819ece5ecb0dc307b79a01908f5cd3923ade9af
KayoticCarnige <kayos156official@gmail.com>
2025-02-11 16:49:31 -0500

    Idk bro

--- web-files/dl/packDL.js
+++ web-files/dl/packDL.js
@@ -20,10 +20,10 @@ fetch("https://mc-sns.github.io/meta.json", {
             if (document.location.pathname == "/dl/" + vList.versions[i].id) {
                 setForDL();
                 console.log(vList.versions[i].id)
-                dlUrl = "https://mc-sns.github.io/versions/" + vList.versions[i].id + "/sticks_n_stones.mrpack"
+                dlUrl = `https://mc-sns.github.io/versions/${vList.versions[i].id}/sticks_n_stones.mrpack`
                 const a = document.createElement('a') // Create <a>
                 a.href = dlUrl
-                a.download = "sticks_n_stones_"+vList.versions[i].id+".mrpack" // File name Here
+                a.download = `sticks_n_stones_${vList.versions[i].id}.mrpack` // File name Here
                 a.click() // Downloaded file
             }
         }
@@ -37,10 +37,10 @@ fetch("https://mc-sns.github.io/meta.json", {
             })
                 .then((response) => response.text())
                 .then((response) => {
-                    dlUrl = "https://mc-sns.github.io/versions/" + response + "/sticks_n_stones.mrpack"
+                    dlUrl = `https://mc-sns.github.io/versions/${response}/sticks_n_stones.mrpack`
                     const a = document.createElement('a') // Create <a>
                     a.href = dlUrl
-                    a.download = "sticks_n_stones_"+response+".mrpack" // File name Here
+                    a.download = `sticks_n_stones_${response}.mrpack` // File name Here
                     a.click() // Downloaded file
                 })
         }
