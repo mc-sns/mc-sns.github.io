45f862da3890b08add906895b0e5555c250f4785
KayoticCarnige <kayos156official@gmail.com>
2025-02-12 02:50:16 -0500

    Problem solved

--- .github/getLatestVersion.js
+++ .github/getLatestVersion.js
@@ -1,6 +1,6 @@
-let vList = JSON.parse(fs.readFileSync(path.join(__dirname,"..","meta.json")));
 const path = require('path');
 const fs = require('fs');
+let vList = require("../meta.json")
 
 
-fs.cpSync(path.join(__dirname,"..","versions",vList.versions[vList.versions.length].id,"vnum.txt"), path.join(__dirname,"..","versions","latest","vnum.txt"), {recursive: true, force: true});
+fs.cpSync(path.join(__dirname,"..","versions",vList.versions[vList.versions.length-1].id,"vnum.txt"), path.join(__dirname,"..","versions","latest","vnum.txt"), {recursive: true, force: true});
