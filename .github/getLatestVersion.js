b980c206117fa5f2035a1fced0528fbe792e18b1
KayoticCarnige <kayos156official@gmail.com>
2025-02-12 02:26:13 -0500

    Simpler and should hopefully work

---
+++ .github/getLatestVersion.js
@@ -0,0 +1,6 @@
+const vList = require("../meta.json")
+const path = require('path');
+const fs = require('fs');
+
+
+fs.cpSync(path.join(__dirname,"..","versions",vList.versions[vList.versions.length].id,"vnum.txt"), path.join(__dirname,"..","versions","latest","vnum.txt"), {recursive: true, force: true});
