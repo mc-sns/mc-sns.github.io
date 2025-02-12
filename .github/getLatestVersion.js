2e53e784667f842ebcc3b998f6a16855f120817d
KayoticCarnige <kayos156official@gmail.com>
2025-02-12 18:31:12 -0500

    Boom

--- .github/getLatestVersion.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-let vList = require("../meta.json")
-
-fs.cpSync(path.join(__dirname,"..","versions",vList.versions[vList.versions.length-1].id,"vnum.txt"), path.join(__dirname,"..","versions","latest","vnum.txt"), {recursive: true, force: true});
