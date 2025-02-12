793756ead14996d916ce69e3e2842563e438ff03
KayoticCarnige <kayos156official@gmail.com>
2025-02-12 02:03:15 -0500

    Update packDL.js

--- web-files/dl/packDL.js
+++ web-files/dl/packDL.js
@@ -1,67 +1,76 @@
 let vList;
 let vListSize;
 let dlPage;
-function setForDL () {
+let latestData;
+function setForDL() {
     dlPage = true;
     document.title = "Getting download..";
-    document.getElementById("pageIco").setAttribute("href", "https://mc-sns.github.io/dlicon.ico");
 }
-fetch("https://mc-sns.github.io/meta.json", {
-    method: "GET",
-    headers: {
-        Accept: "application/json",
-    },
-})
-    .then((response) => response.json())
-    .then((response) => {
-        console.log("Fetched!")
-        vList = response;
-        for (var i = 0; i < vList.versions.length; i++) {
-            if (document.location.pathname == "/dl/" + vList.versions[i].id) {
-                setForDL();
-                console.log(vList.versions[i].id)
-                const a = document.createElement('a') // Create <a>
-                a.href = `https://mc-sns.github.io/versions/${vList.versions[i].id}/modpack_file.mrpack`
-                a.download = `sticks_n_stones_${vList.versions[i].id}.mrpack` // File name Here
-                document.body.appendChild(a)
-                a.click() // Downloaded file
-            }
-        }
-        if (document.location.pathname == "/dl/latest") {
-            setForDL();
-            fetch("https://mc-sns.github.io/versions/latest/vnum.txt", {
-                method: "GET",
-                headers: {
-                    Accept: "application/json",
-                },
-            })
-                .then((response) => response.text())
-                .then((response) => {
+
+if (document.location.pathname.split("/")[1] == "dl") {
+    fetch("https://mc-sns.github.io/meta.json", {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            console.log("Fetched!")
+            vList = response;
+            for (var i = 0; i < vList.versions.length; i++) {
+                if (document.location.pathname == "/dl/" + vList.versions[i].id) {
+                    setForDL();
+                    console.log(vList.versions[i].id)
                     const a = document.createElement('a') // Create <a>
-                    a.href = `https://mc-sns.github.io/versions/${response}/modpack_file.mrpack`
-                    a.download = `sticks_n_stones_${response}.mrpack` // File name Here
+                    a.href = `https://mc-sns.github.io/versions/${vList.versions[i].id}/modpack_file.mrpack`
+                    a.download = `sticks_n_stones_${vList.versions[i].id}.mrpack` // File name Here
                     document.body.appendChild(a)
                     a.click() // Downloaded file
-                })
-        }
-    })
-
-    if (dlPage) {
-        document.getElementById("pageIco").setAttribute("href", "https://mc-sns.github.io/dlicon.ico");
-    } else {
-        document.getElementById("pageIco").setAttribute("href", "https://mc-sns.github.io/favicon.ico");
-        window.onload = () => {
-            setTimeout(() => {
-                if (dlPage) {
                     if (history.back() != undefined) {
                         history.back()
                     } else {
                         window.close()
                     }
-                } else {
-                    document.location = "https://mc-sns.github.io"
                 }
-            }, 1000)
+                if (i+1 == vList.versions.length) {
+                    latestData == JSON.parse(vList.versions[i]);
+                    console.log(latestData.id+" is the latest!")
+                }
+            }
+            if (document.location.pathname == "/dl/latest") {
+                setForDL();
+                fetch("https://mc-sns.github.io/versions/latest/vnum.txt", {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                    },
+                })
+                    .then((response) => response.text())
+                    .then((response) => {
+                        const a = document.createElement('a') // Create <a>
+                        a.href = `https://mc-sns.github.io/versions/${response}/modpack_file.mrpack`
+                        a.download = `sticks_n_stones_${response}.mrpack` // File name Here
+                        document.body.appendChild(a)
+                        a.click() // Downloaded file
+                        if (history.back() != undefined) {
+                            history.back()
+                        } else {
+                            window.close()
+                        }
+                    })
+            }
+        })
+}
+
+if (dlPage) {
+    document.getElementById("pageIco").setAttribute("href", "https://mc-sns.github.io/dlicon.ico");
+} else {
+    document.getElementById("pageIco").setAttribute("href", "https://mc-sns.github.io/favicon.ico");
+    window.onload = () => {
+        setTimeout(() => {
+            document.location = "https://mc-sns.github.io"
+        }, 1000)
     }
 
 }
