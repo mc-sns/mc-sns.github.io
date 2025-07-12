647afee05a53d56c6138f14b533ec1c873d619fa
KiCKTheBucket <kayos156official@gmail.com>
2025-07-11 21:00:05 -0400

    Useless :/

--- timestamp_converter.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-mr_time = "2025-07-06T23:53:45.152058Z"
-dt = datetime.strptime(mr_time, "%Y-%m-%dT%H:%M:%S.%fZ")
-
-log_time = dt.timestamp()
-
-print("Unix Epoch Format:", log_time)
